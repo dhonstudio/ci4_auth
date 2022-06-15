6c79d6caf106b8741f14f66ad344532268a75216
dhonstudio <doon13@gmail.com>
2022-06-15 13:43:17 +0700

    layouting view

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,10 @@ class Home extends BaseController
 {
     public function index()
     {
-        return view('home');
+        $data = [
+            'title' => 'Auth | Dhon Studio'
+        ];
+
+        return view('home', $data);
     }
 }
--- app/Views/home.php
+++ app/Views/home.php
@@ -1,23 +1,6 @@
-<!DOCTYPE html>
+<?= $this->extend('layouts/template'); ?>
 
-<head>
-    <!-- templatemo 418 form pack -->
-    <!-- 
-    Form Pack
-    http://www.templatemo.com/preview/templatemo_418_form_pack 
-    -->
-    <title>Login Two</title>
-    <meta name="keywords" content="" />
-    <meta name="description" content="" />
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet" type="text/css">
-    <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css">
-    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
-    <link href="css/bootstrap-theme.min.css" rel="stylesheet" type="text/css">
-    <link href="css/bootstrap-social.css" rel="stylesheet" type="text/css">
-    <link href="css/templatemo_style.css" rel="stylesheet" type="text/css">
-</head>
+<?= $this->section('content'); ?>
 
 <body class="templatemo-bg-image-1">
     <div class="container">
@@ -91,4 +74,4 @@
     </div>
 </body>
 
-</html>
+<?= $this->endSection(); ?>
